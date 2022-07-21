81a9dfe5d95850dc5f8a8d5a46e3a140254ebbf1
Akash Patil <97583616+akashcodes27@users.noreply.github.com>
2022-07-21 11:51:19 +0530

    Add files via upload

---
+++ vitec/Vitec/README.md
@@ -0,0 +1,20 @@
+# Vivekanand Technologies Website
+>Vivekanand Technologies Website is a static website which fully responsive and built on HTML5, CSS3, JS and Bootstrap4
+## Table of Contents
+* [General Info](#general-information)
+* [Technologies Used](#technologies-used)
+* [Installation](#Installation)
+* [Features](#features)
+* [Screenshots](#screenshots)
+* [Credits](#Credits)
+* [Contact](#contact)
+
+## General Information
+Vivekanand Technologies, is the brain child of two aspiring Indian youngsters with a vision to create a NxtGen Technology company offering software development & maintaince services. We are an Information Technology and Services company with offices in the India.
+
+## Technologies Used
+<a href="https://developer.mozilla.org/en-US/docs/Web/"><img src="https://user-images.githubusercontent.com/83110202/179349420-fefeff4b-952c-4fa5-82e5-81a84f9b00ce.png" height=75 style="padding: 2.5% 2.5%"></a>
+
+## Installation
+```git clone https://github.com/yashshinde03/Synergy-Infotech-Website.git```
+
---
+++ vitec/Vitec/Vivekanand website/Vivekanand/js/main.js
@@ -0,0 +1,152 @@
+(function ($) {
+  "use strict";
+
+  // Preloader (if the #preloader div exists)
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Back to top button
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+    }
+  });
+  $('.back-to-top').click(function(){
+    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // Initiate the wowjs animation library
+  new WOW().init();
+
+  // Header scroll class
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
+
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  // Smooth scroll for the navigation and links with .scrollto classes
+  $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        var top_space = 0;
+
+        if ($('#header').length) {
+          top_space = $('#header').outerHeight();
+
+          if (! $('#header').hasClass('header-scrolled')) {
+            top_space = top_space - 20;
+          }
+        }
+
+        $('html, body').animate({
+          scrollTop: target.offset().top - top_space
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.main-nav, .mobile-nav').length) {
+          $('.main-nav .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+          $('.mobile-nav-overly').fadeOut();
+        }
+        return false;
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.main-nav, .mobile-nav');
+  var main_nav_height = $('#header').outerHeight();
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop();
+  
+    nav_sections.each(function() {
+      var top = $(this).offset().top - main_nav_height,
+          bottom = top + $(this).outerHeight();
+  
+      if (cur_pos >= top && cur_pos <= bottom) {
+        main_nav.find('li').removeClass('active');
+        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
+      }
+    });
+  });
+
+
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+})(jQuery);
+
+
+
+
+
+
+// careers js
+function myFunction1() {
+  document.getElementById("demo1").innerHTML = "&ensp; Please share your resume with hr@synergyinfotech.in";
+  var e2 = document.getElementById('demo1');
+  e2.style.visibility = 'visible'
+}
+function myFunction2() {
+  document.getElementById("demo2").innerHTML = "&ensp; Please share your resume with hr@synergyinfotech.in";
+  var e2 = document.getElementById('demo2');
+  e2.style.visibility = 'visible'
+}
+function myFunction3() {
+  document.getElementById("demo3").innerHTML = "&ensp; Please share your resume with hr@synergyinfotech.in";
+  var e2 = document.getElementById('demo3');
+  e2.style.visibility = 'visible'
+}
+function myFunction4() {
+  document.getElementById("demo4").innerHTML = "&ensp; Please share your resume with hr@synergyinfotech.in";
+  var e2 = document.getElementById('demo4');
+  e2.style.visibility = 'visible'
+}
+
+function myFunctionc1() {
+  var el = document.getElementById('demo1');
+  el.style.visibility = 'hidden'
+}
+
+function myFunctionc2() {
+  var el = document.getElementById('demo2');
+  el.style.visibility = 'hidden'
+}
+
+function myFunctionc3() {
+  var el = document.getElementById('demo3');
+  el.style.visibility = 'hidden'
+}
+
+function myFunctionc4() {
+  var el = document.getElementById('demo4');
+  el.style.visibility = 'hidden'
+}
